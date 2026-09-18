49185ce2b2ea62e604b323326d85b8cb3f794702
Israel Timothy <125442184+Emitis12@users.noreply.github.com>
2026-09-18 15:29:15 +0100

    Update event.js

--- src/data/event.js
+++ src/data/event.js
@@ -40,7 +40,7 @@ export const speakers = [
     name: 'Mr. Emmanuel Maiguwa',
     role: 'President, Alumni of Maritime Academy of Nigeria (AMANO)',
     tag: 'Host',
-    image: '/speaker53.jpg',
+    image: '/speaker53.png',
     //bio: 'Trains the next generation of maritime professionals and advises on academy curriculum reform.',
   },
   {
